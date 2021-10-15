57bd60313a66471a2246f82236088964e9f55f80
Joeri Hermans <joeri@peinser.com>
2021-10-15 02:25:05 +0200

    Update

--- examples/guide/03_parallelism/pipeline.py
+++ examples/guide/03_parallelism/pipeline.py
@@ -5,5 +5,9 @@ In this example we'll explore the potential parallelism.
 import awflow as aw
 
 
+
+# TODO Implement
+
+
 if __name__ == '__main__':
     aw.execute()
---
+++ examples/guide/04_dynamic_graphs/pipeline.py
@@ -0,0 +1,13 @@
+r"""
+In this example we'll build dynamic computational graphs.
+"""
+
+import awflow as aw
+
+
+
+# TODO Implement
+
+
+if __name__ == '__main__':
+    aw.execute()
