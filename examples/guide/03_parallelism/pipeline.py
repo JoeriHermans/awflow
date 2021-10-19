5382b054239217ddf1dcd4f816c1f62a6b1ca945
Joeri Hermans <joeri@peinser.com>
2021-10-19 11:04:40 +0200

    Add parallelism example with 2 distinct approaches to highlight processing throughput

--- examples/guide/03_parallelism/pipeline.py
+++ examples/guide/03_parallelism/pipeline.py
@@ -6,13 +6,24 @@ only be noticable in a Slurm environment, as the standalone
 executor does not currently exploit potential parallelism.
 """
 
+import argparse
 import awflow as aw
 import numpy as np
+import os
 import time
 
 
+parser = argparse.ArgumentParser('awflow guide: Parallelism')
+parser.add_argument('--partition', type=str, default=None, help='Slurm partition to execute workflow on (default: default Slurm partition).')
+parser.add_argument('--reset', action='store_true', default=False, help='Reset the pipeline by clearing all its postconditions (default: false).')
+parser.add_argument('--tasks', type=int, default=25, help='Number of parallel tasks for every stage (default: 25).')
+arguments, _ = parser.parse_known_args()
 
-num_tasks = 10
+
+# Check if files have to be reset
+if arguments.reset:
+    os.system('rm *.npy')
+
 
 ################################################################################
 # Approach 1
@@ -20,40 +31,64 @@ num_tasks = 10
 
 def start_with_tasks():
     start_time = time.time()
-    np.save("starttime-with-tasks.npy", start_time)
+    np.save('starttime-with-tasks.npy', start_time)
 
 
-@aw.tasks(num_tasks)
+@aw.tasks(arguments.tasks)
 @aw.dependency(start_with_tasks)
 def generate_with_tasks(task_index):
-    print("Generating with tasks {} / {}.".format((task_index + 1), num_tasks))
+    print('Generating with tasks {} / {}.'.format((task_index + 1), arguments.tasks))
 
 
-@aw.tasks(num_tasks)
-@aw.dependency(start_with_tasks)
+@aw.tasks(arguments.tasks)
+@aw.dependency(generate_with_tasks)
 def process_after_generation_with_task(task_index):
-    print("Processing after generation with task {} / {}.".format((task_index + 1), num_tasks))
+    print('Processing after generation with task {} / {}.'.format((task_index + 1), arguments.tasks))
 
 
 @aw.dependency(generate_with_tasks)
 @aw.dependency(process_after_generation_with_task)
-@aw.postcondition(aw.exists("time-with-tasks.npy"))  # Don't execute if file exists
+@aw.postcondition(aw.exists('time-with-tasks.npy'))  # Don't execute if file exists
 def after_with_tasks():
-    start_time = np.load("starttime-with-tasks.npy")
+    start_time = np.load('starttime-with-tasks.npy')
     duration = time.time() - start_time
-    print("Duration", duration, "seconds.")
-    np.save("time-with-tasks.npy", duration)
+    print('Duration', duration, 'seconds.')
+    np.save('time-with-tasks.npy', duration)
 
 
-aw.execute()
+aw.execute(partition=arguments.partition)
 
 
 ################################################################################
 # Approach 2
 ################################################################################
 
-# TODO Implement
+def start():
+    start_time = time.time()
+    np.save('starttime.npy', start_time)
 
 
-if __name__ == '__main__':
-    aw.execute()
+dependencies = []
+for task_index in range(arguments.tasks):
+
+    @aw.dependency(start)
+    def generate(task_index=task_index):
+        print('Generating {} / {}.'.format((task_index + 1), arguments.tasks))
+
+    @aw.dependency(generate)
+    def process(task_index=task_index):
+        print('Processing after generation {} / {}.'.format((task_index + 1), arguments.tasks))
+
+    dependencies.append(process)
+
+
+@aw.dependency(dependencies)
+@aw.postcondition(aw.exists('time.npy'))
+def after():
+    start_time = np.load('starttime.npy')
+    duration = time.time() - start_time
+    print('Duration', duration, 'seconds.')
+    np.save('time.npy', duration)
+
+
+aw.execute(partition=arguments.partition)
