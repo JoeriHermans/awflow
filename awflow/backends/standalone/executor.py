cbd676746cfd798376a2deb566478d4c20b85762
Joeri Hermans <joeri@peinser.com>
2021-12-25 12:17:43 +0100

    Remove rich output from standalone backend

--- awflow/backends/standalone/executor.py
+++ awflow/backends/standalone/executor.py
@@ -12,23 +12,19 @@ from awflow.utils.executor import executable_name
 from awflow.utils.executor import generate_executables
 from awflow.utils.executor import generate_metadata
 from awflow.utils.executor import generate_postconditions
-from rich.console import Console
 
 
 
 def execute(workflow: DAWG, dir: str = '.workflows', **kwargs) -> None:
-    console = Console()
     # Preparing the execution files.
     os.makedirs(dir, exist_ok=True)  # Create the base directory
     directory = os.path.abspath(tempfile.mkdtemp(dir=dir))
     program = workflow.program()
     plugins.apply_defaults(workflow=workflow, **kwargs)
     # Generate the necessary files for the graph execution.
-    with console.status("[blue]Preparing compute graph") as status:
-        generate_executables(workflow=workflow, dir=directory)
-    with console.status("[blue]Generating metadata") as status:
-        generate_metadata(workflow=workflow, dir=directory)
-        generate_postconditions(workflow=workflow, dir=directory)
+    generate_executables(workflow=workflow, dir=directory)
+    generate_metadata(workflow=workflow, dir=directory)
+    generate_postconditions(workflow=workflow, dir=directory)
     # Execute the workflow in BFS (program) order.
     program = workflow.program()
     for node in program:
